7cae06bea5ec99ca183da65f65d2dd8a8603f6ac
smckenziebsu <scotty.mckenzie@bsu.edu>
2025-04-10 14:30:45 +0000

    Deployment 1

--- GoalTracking.Console/obj/Debug/net8.0/GoalTracking.Console.AssemblyInfo.cs
+++ GoalTracking.Console/obj/Debug/net8.0/GoalTracking.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoalTracking.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f53b24ccfd304d06e484c8671f187e8781f84947")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d084d1e17e09df75f81580d0f45e39cd8d45135c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoalTracking.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoalTracking.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
